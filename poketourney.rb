5253b40690d656aee722282f0774a782b3247750
Bill Tozier <bill@vagueinnovation.com>
2018-12-25 19:31:33 -0500

    made it daily

--- poketourney.rb
+++ poketourney.rb
@@ -3,13 +3,16 @@ require 'sinatra'
 
 
 get '/' do
+  today = Date.today
+  seed = today.to_time.to_i
+  rando = Random.new(seed)
   types =
-    ["Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel", "Water"].shuffle
+    ["Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel", "Water"].shuffle(random: rando)
 
   p1,p2 = [types[0...9], types[9..-1]]
 
 
 
-  liquid :body, :locals => { :list1 => p1, :list2 => p2 }
+  liquid :body, :locals => { :now => today, :seed => seed, :list1 => p1, :list2 => p2 }
 
 end
